16fdd1b5d4b6ad23bdb54fd19be680b5e9638909
Venkat431 <venkat.aella@gmail.com>
2023-03-25 22:42:33 -0700

    practice

--- roboshop-infra/vars.tf
+++ roboshop-infra/vars.tf
@@ -27,22 +27,22 @@ variable "component" {
     mysql ={
       name="mysql"
       type ="t3.micro"
-      payment ="RoboShop@1"
+      password ="RoboShop@1"
     }
     rabbitmq ={
       name="rabbitmq"
       type ="t3.micro"
-      payment ="roboshop123"
+      password ="roboshop123"
     }
     payment ={
       name="payment"
       type ="t3.micro"
-      payment ="roboshop123"
+      password ="roboshop123"
     }
     shipping ={
       name="shipping"
       type ="t3.micro"
-      payment ="RoboShop@1"
+      password ="RoboShop@1"
     }
   }
 }
