e964f775af2d8d0cf5b45379b0a0ef0ed4faad0b
Venkat431 <venkat.aella@gmail.com>
2023-03-25 22:24:48 -0700

    practice

--- roboshop-infra/vars.tf
+++ roboshop-infra/vars.tf
@@ -12,5 +12,33 @@ variable "component" {
       name="catalogue"
       type ="t3.micro"
     }
+    redis ={
+      name="redis"
+      type ="t3.micro"
+    }
+    user ={
+      name="user"
+      type ="t3.micro"
+    }
+    cart ={
+      name="cart"
+      type ="t3.micro"
+    }
+    mysql ={
+      name="mysql"
+      type ="t3.micro"
+    }
+    rabbitmq ={
+      name="rabbitmq"
+      type ="t3.micro"
+    }
+    payment ={
+      name="payment"
+      type ="t3.micro"
+    }
+    shipping ={
+      name="shipping"
+      type ="t3.micro"
+    }
   }
 }
