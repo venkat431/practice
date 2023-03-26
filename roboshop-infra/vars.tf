0656e8aecdccd311008aa387e32ffdd17bfa417b
Venkat431 <venkat.aella@gmail.com>
2023-03-25 20:27:33 -0700

    practice

--- roboshop-infra/vars.tf
+++ roboshop-infra/vars.tf
@@ -1,14 +1,16 @@
 variable "component" {
-  frontend ={
-    name="frontend"
-    type ="t3.micro"
-  }
-  mongodb ={
-    name="mongodb"
-    type ="t3.micro"
-  }
-  catalogue ={
-    name="catalogue"
-    type ="t3.micro"
+  default = {
+    frontend ={
+      name="frontend"
+      type ="t3.micro"
+    }
+    mongodb ={
+      name="mongodb"
+      type ="t3.micro"
+    }
+    catalogue ={
+      name="catalogue"
+      type ="t3.micro"
+    }
   }
 }
