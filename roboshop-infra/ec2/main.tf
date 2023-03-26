abef521ed51d857958d61de19b3d5429b2477fa5
Venkat431 <venkat.aella@gmail.com>
2023-03-25 21:38:28 -0700

    practice

--- roboshop-infra/ec2/main.tf
+++ roboshop-infra/ec2/main.tf
@@ -20,11 +20,11 @@ resource "null_resource" "provisioner" {
       user     = "centos"
       password = "DevOps321"
     }
-  /*  inline [
+      inline [
       "git clone https://github.com/venkat431/practice",
       "cd roboshop-shell",
       "sudo bash ${var.component}.sh"
-      ]*/
+      ]
   }
 }
 
