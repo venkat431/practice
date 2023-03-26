71d01b588cbc7ff598d64fb8a09338d8e8b73bf3
Venkat431 <venkat.aella@gmail.com>
2023-03-25 20:43:06 -0700

    practice

--- roboshop-infra/ec2/main.tf
+++ roboshop-infra/ec2/main.tf
@@ -1,10 +1,10 @@
-resource "aws_ami" "ami" {
-  most_recent =true
-  name_regex   = "Centos-8-DevOps-Practice"
-  owners =["973714476881"]
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
 }
 resource "aws_instance" "ec2" {
-  ami           = aws_ami.ami.id
+  ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
 
   tags = {
