3ee4559b8ce3c140e9311348134cb3a4564db086
Venkat431 <venkat.aella@gmail.com>
2023-03-25 21:09:55 -0700

    practice

--- roboshop-infra/ec2/main.tf
+++ roboshop-infra/ec2/main.tf
@@ -6,7 +6,7 @@ data "aws_ami" "ami" {
 resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.ami.id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.sg.id]
+  vpc_security_group_ids = [aws_route53_record.route53.id]
   tags                   = {
     Name = var.component
   }
